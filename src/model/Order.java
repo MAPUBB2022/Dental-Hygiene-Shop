848b0799eddc898137eb5352a52b58b6760c3bf6
Irina <avram.irina02@gmail.com>
2022-10-29 21:11:01 +0300

    class Order

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,5 +1,48 @@
 package model;
 
-public class Order {
+import java.time.LocalDateTime;
+import java.util.List;
 
+public class Order {
+    private Integer id;
+    private LocalDateTime dateTime;
+    private Integer userId;
+    private List<ProductOrder> products;
+    private Address deliveryAddress;
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<ProductOrder> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<ProductOrder> products) {
+        this.products = products;
+    }
+
+    public Address getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(Address deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
 }
