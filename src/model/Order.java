bbd873bbb39276ff5ab9d2a5500438828e83bb5c
Irina <avram.irina02@gmail.com>
2022-10-29 21:17:31 +0300

    class Order constructor

--- src/model/Order.java
+++ src/model/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public class Order {
+    public static int idCounter = 0;
     private Integer id;
     private LocalDateTime dateTime;
     private Integer userId;
@@ -44,5 +45,14 @@ public class Order {
 
     public void setDeliveryAddress(Address deliveryAddress) {
         this.deliveryAddress = deliveryAddress;
+    }
+
+    public Order(LocalDateTime dateTime, Integer userId, Address deliveryAddress, List<ProductOrder> products) {
+        idCounter++;
+        this.id = idCounter;
+        this.dateTime = dateTime;
+        this.userId = userId;
+        this.deliveryAddress = deliveryAddress;
+        this.products = products;
     }
 }
