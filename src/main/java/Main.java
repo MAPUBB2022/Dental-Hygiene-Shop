db483c0fdb2b9fdd9b29b10631dd5e0c46d5cadb
Irina <avram.irina02@gmail.com>
2022-12-03 18:45:03 +0200

    finished implementing sorts, filters; modified input handling in menu options;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,14 +9,7 @@ import view.UserView;
 public class Main {
 
     public static void main(String[] args) {
-//        InMemoryUserRepository userRepo = new InMemoryUserRepository();
-//        Controller controller = new Controller(null, null, userRepo);
-//
-//        Integer x = controller.login("ana@yahoo.com", "psswd");
-//        System.out.println(x);
-//
-//        x = controller.login("oana@yahoo.com", "psswd");
-//        System.out.println(x);
+
         InMemoryOrderRepository inMemoryOrderRepository = new InMemoryOrderRepository();
         InMemoryProductRepository inMemoryProductRepository = new InMemoryProductRepository();
         InMemoryUserRepository inMemoryUserRepository = new InMemoryUserRepository();
