08bc0a392bec09b83a5d5c9eaea7f94444061c5f
Bogdan-Nicolae Badea-Calițoiu <96686508+BogdanBCN@users.noreply.github.com>
2023-01-08 23:33:18 +0200

    Controller JavaDocs polish

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -74,8 +74,8 @@ public class Controller {
     /**
      * This method creates an order with the products from a user's shopping cart.
      *
-     * @param user This is the user whose order we create with this method.
-     * @return Order This returns the order we just created.
+     * @param user the user.
+     * @return the order.
      */
     public Order createOrderWithUser(@NotNull User user) {
         List<ProductOrder> products = List.copyOf(userRepository.getCartOfUser(user));
@@ -84,9 +84,10 @@ public class Controller {
 
     /**
      * This method places an order for a user. The order is stored in the order repository and the user's shopping cart is emptied.
+     * The stock of the products that the user orders are reduced accordingly.
      *
-     * @param user  This is the user whose order we place with this method.
-     * @param order This is the order we place with this method.
+     * @param user  the user.
+     * @param order the order.
      */
     public void placeOrderWithUser(@NotNull User user, Order order) {
         orderRepository.placeOrder(user, order);
@@ -100,11 +101,11 @@ public class Controller {
     /**
      * This method logs a user into their account.
      *
-     * @param email    This is the email used in the login attempt.
-     * @param password This is the password used in the login attempt.
-     * @return If the account exists (the email is found) and the password is correct, the user is returned.
-     * @throws IncorrectPasswordException On incorrect password.
-     * @throws UserNotFoundException      On nonexistent email.
+     * @param email    the user's email.
+     * @param password the user's password.
+     * @return the user if their account exists (their email is found) and the password is correct.
+     * @throws IncorrectPasswordException on incorrect password.
+     * @throws UserNotFoundException      on nonexistent email.
      */
     public User login(String email, String password) throws IncorrectPasswordException, UserNotFoundException {
         User user = userRepository.findByEmail(email);
@@ -121,9 +122,9 @@ public class Controller {
     /**
      * This method adds a new product to a user's shopping cart.
      *
-     * @param user      This is the user in whose shopping cart we want to add the product.
-     * @param productId This is the ID of the product we want to add to the user's shopping cart.
-     * @param quantity  This is the amount of pieces of a product we want to add to the user's shopping cart.
+     * @param user      the user.
+     * @param productId the ID of the product.
+     * @param quantity  the quantity of the product.
      */
     public void addNewProductToCart(@NotNull User user, Integer productId, Integer quantity) {
         ProductOrder product = new ProductOrder(productId, quantity,
@@ -132,13 +133,15 @@ public class Controller {
     }
 
     /**
-     * This method adds a product to a user's shopping cart (unlike {@link #addNewProductToCart(User, Integer, Integer)},
-     * it doesn't add a new product, but it modifies the quantity of a product already in the shopping cart).
-     * If the quantity to add is negative, the quantity of the product in the shopping cart will be reduced accordingly.
+     * This method adds a product to a user's shopping cart.
+     * If the product already exists in the shopping cart, the quantity is increased or decreased,
+     * depending on the value of the qtyToAdd variable. If the quantity of the product becomes zero or negative, the
+     * product is removed from the shopping cart.
+     * If the product does not exist in the shopping cart, it is added using the {@link Void addNewProductToCart(User, Integer, Integer)} method.
      *
-     * @param user      the user in whose shopping cart we want to add the product.
-     * @param productId the ID of the product we want to add to the user's shopping cart.
-     * @param qtyToAdd  the amount of pieces of a product we want to add to the user's shopping cart.
+     * @param user      the user.
+     * @param productId the ID of the product.
+     * @param qtyToAdd  the quantity of the product to add.
      * @throws ProductNotInRepositoryException on product ID not matching the ID of a product in the product repository.
      * @throws NegativeQuantityException       on trying to remove a product from the shopping cart that is not inside it.
      * @throws InsufficientStockException      on trying to add more pieces of a product in a shopping cart than there are in stock.
@@ -176,7 +179,7 @@ public class Controller {
     /**
      * This method creates an account by adding a user and their data to the user repository.
      *
-     * @param user This is the account we are creating with this method.
+     * @param user the user.
      */
     public void createAccount(User user) {
         userRepository.add(user);
@@ -186,9 +189,9 @@ public class Controller {
      * This method sorts the products from the product repository by name in ascending or descending order. The sorting
      * is done on a copy list of the product repository.
      *
-     * @param ascending This boolean parameter determines if the products will be sorted by name in ascending or descending
+     * @param ascending this boolean parameter determines if the products will be sorted by name in ascending or descending
      *                  order. If true, the sorting will be in ascending order, otherwise it will be in descending order.
-     * @return A list with the sorted products.
+     * @return a list with the sorted products.
      */
     public List<Product> sortByName(boolean ascending) {
 
@@ -204,9 +207,9 @@ public class Controller {
      * This method sorts the products from the product repository by price in ascending or descending order. The sorting
      * is done on a copy list of the product repository.
      *
-     * @param ascending This boolean parameter determines if the products will be sorted by price in ascending or descending
+     * @param ascending this boolean parameter determines if the products will be sorted by price in ascending or descending
      *                  order. If true, the sorting will be in ascending order, otherwise it will be in descending order.
-     * @return A list with the sorted products.
+     * @return a list with the sorted products.
      */
     public List<Product> sortByPrice(boolean ascending) {
 
@@ -221,8 +224,8 @@ public class Controller {
     /**
      * This method filters the products from the product repository based on if they contain a given string.
      *
-     * @param text This is the string by which the filtering is done.
-     * @return A list of the products containing the given string.
+     * @param text the string.
+     * @return a list of the products containing the given string.
      */
     public List<Product> filterByHasInName(String text) {
         return new java.util.ArrayList<>(List.copyOf(productRepository.getProductList())).
@@ -237,8 +240,8 @@ public class Controller {
     /**
      * This method filters the products from the product repository based on if they are of a given type.
      *
-     * @param type This is the type by which the filtering is done.
-     * @return A list of the products of the given type.
+     * @param type the type.
+     * @return a list of the products of the given type.
      */
     public List<Product> filterByType(ProductType type) {
         return new java.util.ArrayList<>(List.copyOf(productRepository.getProductList())).
@@ -248,8 +251,8 @@ public class Controller {
     /**
      * This method filters the products from the product repository based on if they are of a given use.
      *
-     * @param use This is the use by which the filtering is done.
-     * @return A list of the products of the given use.
+     * @param use the use.
+     * @return a list of the products of the given use.
      */
     public List<Product> filterByUse(ProductUse use) {
         return new java.util.ArrayList<>(List.copyOf(productRepository.getProductList())).
@@ -258,11 +261,11 @@ public class Controller {
 
     /**
      * This method finds a product by ID and replaces it with another product with the same attributes, except the
-     * attribute/s we want to modify.
+     * attribute/s to modify.
      *
-     * @param productId  This is the product we want to modify.
-     * @param newProduct This is the product that replaces the product that we want to modify.
-     * @throws ProductNotInRepositoryException On product ID not matching the ID of a product in the product repository.
+     * @param productId  the ID of the product.
+     * @param newProduct the replacement product.
+     * @throws ProductNotInRepositoryException on product ID not matching the ID of a product in the product repository.
      */
     public void modifyProduct(int productId, Product newProduct) throws ProductNotInRepositoryException {
         Product searched = productRepository.findById(productId);
@@ -273,11 +276,11 @@ public class Controller {
     }
 
     /**
-     * This method searches a product by ID.
+     * This method searches for a product by ID.
      *
-     * @param productId This is the ID of the product we are looking for.
-     * @return The found product or null if the product is not found.
-     * @throws ProductNotInRepositoryException On product ID not matching the ID of a product in the product repository.
+     * @param productId the ID of the product.
+     * @return the product or null if the product is not found.
+     * @throws ProductNotInRepositoryException on product ID not matching the ID of a product in the product repository.
      */
     public Product findProductById(int productId) throws ProductNotInRepositoryException {
         Product searched = productRepository.findById(productId);
@@ -288,11 +291,11 @@ public class Controller {
     }
 
     /**
-     * This method finds an order by ID and modifies its delivery address.
+     * This method searches for an order by ID and modifies its delivery address if found.
      *
-     * @param orderId This is the ID of the order we are looking for.
-     * @param address This is the address that we are going to replace the old one with.
-     * @throws OrderNotInRepositoryException On order ID not matching the ID of an order in the product repository.
+     * @param orderId the ID of the order.
+     * @param address the new delivery address.
+     * @throws OrderNotInRepositoryException on order ID not matching the ID of an order in the product repository.
      */
     public void modifyOrderDeliveryAddress(Integer orderId, Address address)
             throws OrderNotInRepositoryException {
@@ -304,12 +307,12 @@ public class Controller {
     }
 
     /**
-     * This method finds an order by ID and removes a certain product in whichever amount it is present there.
+     * This method searches for an order by ID and removes a product from it in whichever amount it is present there.
      *
-     * @param orderId   This is the ID of the order we are looking for.
-     * @param productId This is the ID of the product we want to remove.
-     * @throws OrderNotInRepositoryException   On order ID not matching the ID of an order in the product repository.
-     * @throws ProductNotInRepositoryException On product ID not matching the ID of a product in the product repository.
+     * @param orderId   the ID of the sought order.
+     * @param productId the ID of the sought product.
+     * @throws OrderNotInRepositoryException   on order ID not matching the ID of an order in the product repository.
+     * @throws ProductNotInRepositoryException on product ID not matching the ID of a product in the product repository.
      */
     public void modifyOrderProductList(Integer orderId, Integer productId)
             throws OrderNotInRepositoryException, ProductNotInRepositoryException {
@@ -324,46 +327,102 @@ public class Controller {
         orderRepository.modifyProducts(orderId, productId);
     }
 
+    /**
+     * This method adds a product to the product repository.
+     *
+     * @param product the product.
+     */
     public void addProduct(Product product) {
         productRepository.add(product);
     }
 
+    /**
+     * This method deletes a product from the product repository.
+     *
+     * @param id the ID of the product.
+     */
     public void deleteProduct(Integer id) {
         productRepository.delete(id);
     }
 
+    /**
+     * This method empties the cart of a user.
+     *
+     * @param user the user.
+     */
     public void emptyCart(User user) {
         userRepository.emptyCart(user);
     }
 
+    /**
+     * User cart getter.
+     */
     public List<ProductOrder> getUserCart(User user) {
         return userRepository.getCartOfUser(user);
     }
 
+    /**
+     * Order history getter.
+     */
     public List<Order> getOrderHistoryOfUser(User user) {
         return userRepository.getOrderHistoryOfUser(user);
     }
 
-    public void deleteAccountById(Integer id){
+    /**
+     * This method deletes a user from the user repository.
+     *
+     * @param id the id of the user.
+     */
+    public void deleteAccountById(Integer id) {
         userRepository.delete(id);
     }
 
+    /**
+     * This method modifies the delivery address of a user.
+     *
+     * @param user       the user.
+     * @param newAddress the new address.
+     */
     public void modifyUserDeliveryAddress(User user, Address newAddress) {
         userRepository.modifyDeliveryAddress(user, newAddress);
     }
 
+    /**
+     * This method modifies the name of a user.
+     *
+     * @param user the user.
+     * @param name the new name.
+     */
     public void modifyUserName(User user, String name) {
         userRepository.modifyName(user.getId(), name);
     }
 
+    /**
+     * This method modifies the email of a user.
+     *
+     * @param user  the user.
+     * @param email the new email.
+     */
     public void modifyUserEmail(User user, String email) {
         userRepository.modifyEmail(user.getId(), email);
     }
 
+    /**
+     * This method modifies the password of a user.
+     *
+     * @param user     the user.
+     * @param password the new password.
+     */
     public void modifyUserPassword(User user, String password) {
         userRepository.modifyPassword(user.getId(), password);
     }
 
+    /**
+     * This method modifies the phone number of a user.
+     *
+     * @param user        the user.
+     * @param phoneNumber the new phone number.
+     */
     public void modifyUserPhoneNumber(User user, String phoneNumber) {
         userRepository.modifyPhoneNumber(user.getId(), phoneNumber);
     }
